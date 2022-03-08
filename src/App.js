6e3888e81be0d4dcbd940b875f93f89d07ecc18a
Patrick Müller <p.moe.mueller@gmail.com>
2022-03-08 12:59:48 +0100

    Create layout in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Input from './components/Input';
+import Button from './components/Button';
+// import Player from './components/Player'
+
 function App() {
-  return <h1>Scorekeeper</h1>;
+  return (
+    <main>
+      <Input label="Name fo game" placeholder="e.g. Carcassonne" />
+      <Input label="Player nanmes" placeholder="e.g. Jon Doe, Jane Doe" />
+      <Button children="Create game" />
+      <Button children="Play" isDark="true" />
+      <Button children="History" />
+    </main>
+  );
 }
 
 export default App;
